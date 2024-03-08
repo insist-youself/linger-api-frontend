7d0b8e71369f5a1c59891653f021b8c649e925be
unwilling to leave <1433656186@qq.com>
2024-03-08 14:30:22 +0800

    添加用户角色判断语句

--- src/access.ts
+++ src/access.ts
@@ -4,8 +4,6 @@
 export default function access(initialState: InitialState | undefined) {
   const { loginUser } = initialState ?? {};
   return {
-    canUser: loginUser,
-    // 如果loginUser存在，并且用户角色为 'admin', 说明该用户是管理员
-    canAdmin: loginUser?.userRole === 'admin',
+    canAdmin: loginUser && loginUser.userRole === 'admin',
   };
 }
